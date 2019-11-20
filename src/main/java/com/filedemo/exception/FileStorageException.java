f0a8e76435f33c8aa507a8bdf82300dac8c430c4
Sanjit Shrestha <forofficialsanjit@gmail.com>
2019-11-20 10:58:41 +0545

    upload system

---
+++ src/main/java/com/filedemo/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.filedemo.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
